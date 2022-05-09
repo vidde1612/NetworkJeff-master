496226dd609c38c484afe341301e5813bcbb99c6
Vidar Erkendal <Vidar.erkendal@gmail.com>
2022-05-09 08:48:29 +0200

    Init commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerController : NetworkBehaviour
+{
+
+    [SerializeField]
+    float movementSpeed = 3f; // Unity-enheter per sekund
+
+    [SerializeField]
+    float rotationSpeed = 150f; // Grader per sekund
+
+    [SerializeField]
+    GameObject bulletPrefab;
+
+    [SerializeField]
+    Transform bulletSpawnPoint;
+
+    [SerializeField]
+    float timeBetweenShots = 0.5f;
+    float timeSinceLastShot = 0f;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        float yRotation = Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime;
+        float zMovement = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
+
+        Vector3 rotationVector = new Vector3(0, yRotation, 0);
+        Vector3 movementVector = new Vector3(0, 0, zMovement);
+
+        transform.Rotate(rotationVector);
+        transform.Translate(movementVector);
+
+        timeSinceLastShot += Time.deltaTime;
+
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            if (timeSinceLastShot > timeBetweenShots)
+            {
+                Fire();
+                timeSinceLastShot = 0;
+            }
+        }
+
+    }
+
+    void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+    }
+}
